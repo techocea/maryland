22c5bc5f08dd103734fa8c148525a3d26a605515
techocea <dunsfordbright@gmail.com>
2025-04-02 12:32:06 +0530

    updated dependencies4

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 export { auth as middleware } from "@/auth";
 
 export const config = {
+  unstable_allowDynamic: [
+    "/lib/mongodb.ts", // allows a single file
+    "**/node_modules/function-bind/**", // use a glob to allow anything in the function-bind 3rd party module
+  ],
   matcher: ["/dashboard/:path*)"],
 };
